4d813fdeb7152769fe05d9071d05c48cd13dc911
Nityanand Katare <iknityanand@gmail.com>
2017-07-11 20:38:34 +0530

    first-component
    added a new statefull component. understood the difference between sate vs statefull function and component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const App = () => <h1>Hi</h1>
-
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>{this.props.txt}</h1>
+        <b>watching you!</b>
+      </div>
+      )
+  }
+}
 export default App
