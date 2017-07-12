f5debaf951a899545142d1876e05d958af042cd3
Nityanand Katare <iknityanand@gmail.com>
2017-07-12 21:22:28 +0530

    multiple-components
    multiple components ie; two component are rendered App  and AppWithState.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AppWithState from './AppWithState';
 import registerServiceWorker from './registerServiceWorker';
 
+let allComponents= (
+	<div>
+		<App cat={50} />
+		<AppWithState />
+	</div>
+);
+
 ReactDOM.render(
-	<App cat={50} />, 
+	allComponents,
 	document.getElementById('root')
 	);
 registerServiceWorker();
