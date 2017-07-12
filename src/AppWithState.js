1c4bacdd677687b3e91a68f1b622405396479fb7
Nityanand Katare <iknityanand@gmail.com>
2017-07-12 22:20:15 +0530

    Adding Children component
    Add Widget children component

--- src/AppWithState.js
+++ src/AppWithState.js
@@ -1,7 +1,24 @@
 import React from 'react'
 class AppWithState extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      txt: 'This is a state text'
+    }
+  }
+
+  update( e ){
+    this.setState({txt: e.target.value})
+  }
   render(){
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <h1>{this.state.txt}</h1>
+        <Widget update={this.update.bind(this)}/>
+      </div>
+    );
   }
 }
+
+const Widget = (props) => <input type="text" onChange={props.update} />
 export default AppWithState
